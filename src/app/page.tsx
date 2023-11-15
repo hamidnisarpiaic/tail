4b1d18eee7e1cf9c04f4acc0cc36c87bb4fa5fb1
Hamid Nisar <hamidnisarpiaic@gmail.com>
2023-11-15 09:23:12 +0500

    UPDATE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ export default function Home() {
     <>
     <div className="flex justify-center items-center bg-slate-400 m-10 p-3">
     <div className="w-2/3 text-center">
-        <h1 className="text-white text-2xl tracking-widest mx-auto"> Welcome to Panaverse</h1>
-       <h2 className="text-white"> Nice to see you</h2>
-       <button className="border border-double rounded p-2 m-5 bg-blue-800 text-white hover:bg-red-900"> Click Me</button>
+        <h1 className="text-white text-2xl tracking-widest mx-auto">Hamid Nisar</h1>
+       <h2 className="text-white">Web Developer</h2>
+       <button className="border border-double rounded p-2 m-5 bg-blue-800 text-white hover:bg-red-900">Learn More</button>
     </div>
     
-    <div>
-    <Image src="/h.png" alt="Hamid Logo" width={150} height={150} className="rounded-full p-3" />
+    <div className="border-4 rounded-full border-white">
+    <Image src="/h.png" alt="Hamid Logo" width={120} height={120} className="rounded-full p-3" />
+    <div className="flex text-white justify-center p-0 font-serif">Hamid Nisar</div>
     </div>
 
     </div>
